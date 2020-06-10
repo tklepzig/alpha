755b56b5aef22a7825731bbc0f57c5e3210e40be
Thomas Klepzig <thomas.klepzig@invision.de>
2020-06-10 21:17:33 +0200

    Only cache listed urls, not newly created requests
    Otherwise api responses like /lists would be cached as well

--- public/sw.js
+++ public/sw.js
@@ -1,29 +1,32 @@
-var CACHE_NAME = "alpha-cache-2020-03-13";
+var CACHE_NAME = "alpha-cache-2020-06-10";
 var urlsToCache = [
   "/",
   "manifest.json",
   "/style.css",
   "/sw.js",
+  "/components/ConnectionIndicator.js",
   "/common.js",
-  "/app.js"
+  "/app.js",
+  "https://fonts.googleapis.com/css?family=Open+Sans",
+  "https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js",
 ];
 
-self.addEventListener("install", function(event) {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
+    caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(function (response) {
       if (response) {
         return response;
       }
 
-      return fetch(event.request).then(function(response) {
+      return fetch(event.request).then(function (response) {
         if (
           !response ||
           response.status !== 200 ||
@@ -32,10 +35,11 @@ self.addEventListener("fetch", function(event) {
           return response;
         }
 
-        var responseToCache = response.clone();
-        caches.open(CACHE_NAME).then(function(cache) {
-          cache.put(event.request, responseToCache);
-        });
+        // Do not cache newly created requests which are not listed above
+        //var responseToCache = response.clone();
+        //caches.open(CACHE_NAME).then(function(cache) {
+        //cache.put(event.request, responseToCache);
+        //});
 
         return response;
       });
